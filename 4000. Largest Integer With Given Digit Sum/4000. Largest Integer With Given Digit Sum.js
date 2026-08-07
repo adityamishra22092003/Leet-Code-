bcdb216a0f56d905906a6cf5b273c18d6330d1d4
Aditya Mishra <adimishra2209@gmail.com>
2026-08-07 19:18:49 +0530

    [4000] [Time Beats: 100%] [Memory Beats: 100.00%] - Leet2Hub

--- 4000. Largest Integer With Given Digit Sum/4000. Largest Integer With Given Digit Sum.js
+++ 4000. Largest Integer With Given Digit Sum/4000. Largest Integer With Given Digit Sum.js
@@ -1,1 +1,6 @@
-const largestInteger = (n, s) => {    if (s > n * 9) return -1;    const q = (s / 9) | 0;    return 10 ** n - 10 ** (n - q) + (s % 9) * 10 ** (n - q - 1);};
+1const largestInteger = (n, s) => {
+2    if (s > n * 9) return -1;
+3    const q = (s / 9) | 0;
+4
+5    return 10 ** n - 10 ** (n - q) + (s % 9) * 10 ** (n - q - 1);
+6};
