ebbeb65647bafdcaeddbbf09a0d3a049ffbee486
Aditya Mishra <adimishra2209@gmail.com>
2026-08-07 19:21:01 +0530

    [4000] [Time Beats: 100%] [Memory Beats: 100.00%] - Leet2Hub

--- 4000. Largest Integer With Given Digit Sum/4000. Largest Integer With Given Digit Sum.cpp
+++ 4000. Largest Integer With Given Digit Sum/4000. Largest Integer With Given Digit Sum.cpp
@@ -1,21 +1,12 @@
 1class Solution {
 2public:
 3    int largestInteger(int n, int s) {
-4        if(s>9*n)return -1;
-5        if(s==0)return 0;
-6        int ans=0;
-7        while(n--){
-8            if(s>=9){
-9                ans+=9;
-10                s-=9;
-11            }
-12            else{
-13                ans+=s;
-14                s=0;
-15            }
-16            
-17            ans*=10;
-18        }
-19        return ans/10;
-20    }
-21};
+4        int ans = 0;
+5        while(n--) {
+6            ans = ans * 10 + min(9, s);
+7            s -= min(9, s);
+8        }
+9        if(s) return -1;
+10        return ans;
+11    }
+12};
